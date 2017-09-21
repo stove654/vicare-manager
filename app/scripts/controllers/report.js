00f7c7c469511876142d5290d3e6fbc4e5121514
Loi Hoang <loihoang@Lois-MacBook-Pro.local>
2017-09-21 16:24:57 +0700

    update code

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -10,8 +10,47 @@
 angular.module('appManager')
   .controller('ReportCtrl', function ($scope, $http, config, $state, localStorageService) {
 
+    var d = new Date();
+    d.setDate(d.getDate() - 7);
+    console.log(d.toString());
+
     $scope.data = {
-      reports: []
+      reports: [],
+      selectFilter: '1',
+      from: d,
+      to: new Date()
+    };
+
+    console.log($scope.data.from);
+
+    $scope.open1 = function () {
+      $scope.popup1.opened = true;
+    };
+
+    $scope.popup1 = {
+      opened: false
+    };
+
+    $scope.popup2 = {
+      opened: false
+    };
+
+    $scope.dateOptions1 = {
+      formatYear: 'yy',
+      maxDate: new Date()
+    };
+
+    $scope.open2 = function () {
+      $scope.popup2.opened = true;
+    };
+
+    $scope.popup2 = {
+      opened: false
+    };
+
+    $scope.dateOptions2 = {
+      formatYear: 'yy',
+      maxDate: new Date()
     };
 
     $scope.goHistoryChat = function (id, item) {
@@ -19,16 +58,32 @@ angular.module('appManager')
       localStorageService.set('reportDetail', item)
     };
 
+    $scope.selectDataFilter = function (filter) {
+      var query = {
+        filter: filter,
+        from: $scope.data.from,
+        to: $scope.data.to
+      };
+
+      console.log(query)
+      $http.get(config.url + config.api.report, {
+        params: query
+      }).then(function (response) {
+        $scope.data.reports = response.data;
+      });
+    };
+
+    $scope.$watch('data.from', function () {
+      $scope.selectDataFilter($scope.data.selectFilter);
+    });
+
+    $scope.$watch('data.to', function () {
+      $scope.selectDataFilter($scope.data.selectFilter);
+    });
+
     var _init = function () {
 
-      $http.get(config.url + config.api.report).then(function (response) {
-        // angular.forEach(response.data, function (value) {
-        //   value.fromProfile = JSON.parse(value.fromProfile);
-        //   value.toProfile = JSON.parse(value.toProfile);
-        // })
-        $scope.data.reports = response.data;
-        console.log(response.data)
-      });
+      $scope.selectDataFilter($scope.data.selectFilter);
     };
 
     _init();
